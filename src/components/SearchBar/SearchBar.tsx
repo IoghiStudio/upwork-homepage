eaf114f15d229941e4c88ab068758c83f17d9a73
Nicusor Iorga <ioghigithublab@gmail.com>
2023-04-24 18:29:53 +0300

    working on header

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,7 +4,10 @@ import './SearchBar.scss';
 export const SearchBar = () => {
   return (
     <div className="searchbar">
-
+      <input
+        className='searchbar__input'
+        type="text"
+      />
     </div>
   );
 }
